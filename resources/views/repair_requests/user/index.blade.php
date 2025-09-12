96627f59a2d506b220e1ae4a1e1ce191f5ef7435
ตอง คับ <tonghaha2001@gmail.com>
2025-09-12 13:34:52 +0700

    .

--- resources/views/repair_requests/user/index.blade.php
+++ resources/views/repair_requests/user/index.blade.php
@@ -92,8 +92,6 @@
                             class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700">
                             Back to Dashboard
                         </a>
-
-                        {{-- ปุ่ม New Repair Request เดิม --}}
                         <a href="{{ route('repair_requests.create') }}"
                             class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700">
                             + New Repair Request
